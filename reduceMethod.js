071fb937583d4955e0849b8364f571229720c78d
Lewis Betances <patience174@hotmail.com>
2018-07-22 03:56:07 -0700

    Reduce Method

---
+++ reduceMethod.js
@@ -0,0 +1,15 @@
+reduceMethod = [1,2,3,4];
+var ini = 0;
+
+for(let i=0;i<reduceMethod.length;i++){
+  ini+= reduceMethod[i];
+}console.log(ini);//10
+
+let ini2=0;
+for (let theArray of reduceMethod){
+  ini2 += theArray;  
+}console.log(ini2);//10
+
+
+var result = reduceMethod.reduce((initilizer,myArray) => initilizer+myArray);
+console.log(result);//10
